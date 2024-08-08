0821ecff150bf5e7ac74b6ef776da199bde6811e
sivm99 <git.v7gr4@simplelogin.fr>
2024-08-08 17:10:49 +0530

    logo added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -14,7 +14,11 @@ function Header() {
 
     return (
         <header className="head--main flex p-5 items-center justify-between shadow-xl rounded-none border-gray-600 mb-4 pl-4 pr-4">
-            <h1 className="colo text-white text-center text-7xl font-semibold">Memed</h1>
+            <a href="/" className="flex">
+            <img src="/tl.png" alt="Memed" className="w-20 h-20 bg-orange-50 rounded-3xl mr-3"/>
+
+            <h2 className="colo text-white text-center text-7xl font-semibold">Memed</h2>
+            </a>
             <h3 className="greeting text-white text-2xl">Good {greeting && greeting()}</h3>
         </header>
     )
